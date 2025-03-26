6f5ea1769d87c50aef118e1782198ddc9b6297c8
Wendell <wendellbaresi93@gmail.com>
2025-03-26 17:40:42 -0300

    refactor: Modifica classe JogadorService para JogadorServiceImpl para passar os principios solid

--- src/main/java/com/projetofinal/escolinha/service/JogadorServiceImpl.java
+++ src/main/java/com/projetofinal/escolinha/service/JogadorServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class JogadorService {
+public class JogadorServiceImpl {
 
     @Autowired
     private JogadorRepository jogadorRepository;
