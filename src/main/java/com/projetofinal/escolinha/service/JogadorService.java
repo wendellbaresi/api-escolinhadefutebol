7d332e181acc505574c14ae76730f2fb364d4b9e
Wendell <wendellbaresi93@gmail.com>
2025-03-26 17:50:17 -0300

    feat: Cria interface JogadorService para melhorar a seguranca da aplicacao

---
+++ src/main/java/com/projetofinal/escolinha/service/JogadorService.java
@@ -0,0 +1,13 @@
+package com.projetofinal.escolinha.service;
+
+import com.projetofinal.escolinha.model.Jogador;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JogadorService {
+    List<Jogador> listarjogadores();
+    Jogador cadastrarjogador(Jogador jogador);
+    Optional<Jogador> listarJogador(Integer id);
+    void removerJogador(Integer id);
+}
