a6f0cd8e2ceb3d03d5cd4c1471a75122addfac8d
Wendell <wendellbaresi93@gmail.com>
2025-06-03 14:51:04 -0300

    refact: Update model class Player, add anotations

--- src/main/java/com/projetofinal/escolinha/model/Player.java
+++ src/main/java/com/projetofinal/escolinha/model/Player.java
@@ -1,104 +1,34 @@
 package com.projetofinal.escolinha.model;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "jogador")
+@Table(name = "player")
 public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Integer age;
+    @Column(nullable = false)
+    private Double height;
+    @Column(nullable = false)
+    private Double weight;
+    @Column(nullable = false)
+    private String dominantLeg;
+    @Column(nullable = false)
+    private String position;
 
-    @Column
-    private String nome;
-    @Column
-    private Integer idade;
-    @Column
-    private Double altura;
-    @Column
-    private Double peso;
-    @Column
-    private String perna;
-    @Column
-    private String posicao;
-
-    @ManyToOne // Relaçao de Muitos para Um
+    @ManyToOne
     @JoinColumn(name = "id_categoria")
     private Category category;
-
-
-
-    public Player(){
-
-    }
-
-
-
-
-
-    // Metodos Getter e Setter
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public Double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(Double altura) {
-        this.altura = altura;
-    }
-
-    public Double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(Double peso) {
-        this.peso = peso;
-    }
-
-    public String getPerna() {
-        return perna;
-    }
-
-    public void setPerna(String perna) {
-        this.perna = perna;
-    }
-
-    public String getPosicao() {
-        return posicao;
-    }
-
-    public void setPosicao(String posicao) {
-        this.posicao = posicao;
-    }
-
-    public Category getCategoria() {
-        return category;
-    }
-
-    public void setCategoria(Category category) {
-        this.category = category;
-    }
 }
